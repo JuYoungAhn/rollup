0b2a6b866b306d1f5b03e2ca10003d4b97881831
dan-ahn <dan.ahn@kakaocorp.com>
2024-08-28 14:41:55 +0900

    pkgdown

--- R/rollup.R
+++ R/rollup.R
@@ -1,13 +1,13 @@
 
 #' grouped_df_list class definition
-#' 
+#'
 #' A class to represent a list of grouped data frames.
-#' 
+#'
 #' @export
 setClass("grouped_df_list", representation(df_list = "list"))
 
 #' Generic `summarize` function
-#' 
+#'
 #' @param object Object to be summarized.
 #' @param ... Additional arguments.
 #' @return An object of the same class as `.data`. One grouping level will be dropped.
@@ -28,7 +28,7 @@ setMethod("summarize", "ANY", function(object, ...) {
 
 
 #' Method for `summarize` on grouped_df_list
-#' 
+#'
 #' @param object A grouped_df_list object.
 #' @param ... Additional arguments.
 #' @return An object of the same class as `.data`. One grouping level will be dropped.
@@ -38,7 +38,7 @@ setMethod("summarize", signature(object = "grouped_df_list"), function(object, .
 })
 
 #' Generic `summarise` function
-#' 
+#'
 #' @param object Object to be summarized.
 #' @param ... Additional arguments.
 #' @return An object of the same class as `.data`. One grouping level will be dropped.
@@ -48,7 +48,7 @@ setGeneric("summarise", function(object, ...) {
 })
 
 #' Default method for `summarise`
-#' 
+#'
 #' @param object An object
 #' @param ... Additional arguments.
 #' @return An object of the same class as `.data`. One grouping level will be dropped.
@@ -58,7 +58,7 @@ setMethod("summarise", "ANY", function(object, ...) {
 })
 
 #' Method for `summarise` on grouped_df_list
-#' 
+#'
 #' @param object A grouped_df_list object.
 #' @param ... Additional arguments.
 #' @return An object of the same class as `.data.` One grouping level will be dropped.
@@ -69,7 +69,7 @@ setMethod("summarise", signature(object = "grouped_df_list"), function(object, .
 
 #' grouping_sets
 #'
-#' Compute total amounts at different group levels, producing multiple subtotals. With the 'grouping_sets' clause following 'group_by', you can aggregate multiple grouping variables in one operation. This reflects the 'GROUPING SETS' operations in 'SQL'. 
+#' Compute total amounts at different group levels, producing multiple subtotals. With the 'grouping_sets' clause following 'group_by', you can aggregate multiple grouping variables in one operation. This reflects the 'GROUPING SETS' operations in 'SQL'.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom methods new
@@ -78,13 +78,13 @@ setMethod("summarise", signature(object = "grouped_df_list"), function(object, .
 #' @import dplyr
 #' @usage grouping_sets(df, ...)
 #' @param df dataframe or grouped df
-#' @param ... grouping variables 
+#' @param ... grouping variables
 #' @return A list of 'grouped_df' class. each 'grouped_df' object has a different grouping level.
 #' @examples
-#' mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am")) 
-#' mtcars %>% group_by(vs, am) %>% with_rollup() 
-#' mtcars %>% group_by(vs, am) %>% with_cube() 
-#' @export 
+#' mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am"))
+#' mtcars %>% group_by(vs, am) %>% with_rollup()
+#' mtcars %>% group_by(vs, am) %>% with_cube()
+#' @export
 grouping_sets <- function(df, ...) {
   var_list <- list(...)
   df <- df %>% ungroup()
@@ -111,9 +111,9 @@ grouping_sets <- function(df, ...) {
 #' @return A list of 'grouped_df' class. each 'grouped_df' object has a different grouping level.
 #' @examples
 #' mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am"))
-#' mtcars %>% group_by(vs, am) %>% with_rollup() 
-#' mtcars %>% group_by(vs, am) %>% with_cube() 
-#' @export 
+#' mtcars %>% group_by(vs, am) %>% with_rollup()
+#' mtcars %>% group_by(vs, am) %>% with_cube()
+#' @export
 with_cube <- function(grouped_df) {
   grouping_vars <- grouped_df %>% dplyr::group_vars()
   combinations <- unlist(lapply(seq_along(grouping_vars), function(x) combn(grouping_vars, x, simplify = FALSE)), recursive = FALSE)
@@ -129,21 +129,21 @@ with_cube <- function(grouped_df) {
 #' @param grouped_df 'grouped_df' class
 #' @return A list of 'grouped_df' class. each 'grouped_df' object has a different grouping level.
 #' @examples
-#' mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am")) 
-#' mtcars %>% group_by(vs, am) %>% with_rollup() 
-#' mtcars %>% group_by(vs, am) %>% with_cube() 
+#' mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am"))
+#' mtcars %>% group_by(vs, am) %>% with_rollup()
+#' mtcars %>% group_by(vs, am) %>% with_cube()
 #' @export
 with_rollup <- function(grouped_df) {
   grouping_vars <- grouped_df %>% dplyr::group_vars()
   grouped_var_list <- list()
-  
+
   for (i in seq_along(grouping_vars)) {
     grouped_var_list[[i]] <- grouping_vars[1:(length(grouping_vars) - i + 1)]
   }
-  
+
   grouped_var_list[[length(grouping_vars) + 1]] <- NA
   do.call(grouping_sets, c(list(grouped_df), grouped_var_list))
-} 
+}
 
 #' summarize_rollup
 #'
@@ -156,24 +156,24 @@ with_rollup <- function(grouped_df) {
 #' @param ... functions for 'summarize'
 #'
 #' @return An object of the same class as `.data`. The unioned aggregated result of multiple grouping levels will be dropped.
-#' @export 
-summarize_rollup <- function(df_list, ...) {  
-  funcs <- rlang::quos(...)  
+#' @export
+summarize_rollup <- function(df_list, ...) {
+  funcs <- rlang::quos(...)
 
   result_tmp <- lapply(df_list, function(x) {
     summarized_df <- dplyr::summarize(x, !!!funcs)
   })
-  
+
   common_cols <- Reduce(intersect, lapply(result_tmp, colnames))
-  
-  if(inherits(df_list[[1]], "tbl_spark")) {  
+
+  if(inherits(df_list[[1]], "tbl_spark")) {
     if(requireNamespace("sparklyr", quietly = TRUE)) {
-      binded_result <- sparklyr::sdf_bind_rows(result_tmp)  
+      binded_result <- sparklyr::sdf_bind_rows(result_tmp)
     }
     else {
       stop("sparklyr package is required for operating on Spark DataFrames.")
     }
-  }  
+  }
   else {
     binded_result <- dplyr::bind_rows(result_tmp)
   }
@@ -182,7 +182,7 @@ summarize_rollup <- function(df_list, ...) {
   exclude_common_cols <- cols[!cols %in% common_cols]
   binded_result <- binded_result %>% select(all_of(c(exclude_common_cols, common_cols)))
   return(binded_result)
-} 
+}
 
 
 
--- README.md
+++ README.md
@@ -16,10 +16,16 @@ devtools::install_github("JuYoungAhn/rollup")
 [![CRAN\_Download\_Badge](https://cranlogs.r-pkg.org/badges/rollup)](https://CRAN.R-project.org/package=rollup)
 
 
-## Usage
-- [tutorial link](https://juyoungahn.github.io/rollup/articles/tutorial.html)
+## In a Nutshell
+- If you want to learn more about `rollup`, please refer to the [Tutorial](https://juyoungahn.github.io/rollup/articles/tutorial.html).
 ```r
-mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am"),NA)
-mtcars %>% group_by(vs, am) %>% with_rollup() 
-mtcars %>% group_by(vs, am) %>% with_cube() 
+mtcars %>% group_by(vs, am) %>% grouping_sets("vs","am",c("vs","am"),NA) %>% 
+  summarize(n=n(), avg_mpg=mean(mpg))
+
+mtcars %>% group_by(vs, am) %>% with_rollup() %>% 
+  summarize(n=n(), avg_mpg=mean(mpg))
+
+mtcars %>% group_by(vs, am) %>% with_cube() %>% 
+  summarize(n=n(), avg_mpg=mean(mpg))
+
 ```
